0daecb4ec43f0bd2354a7d0f7eff2cac0c025881
iweaklim <iweaklim@CoreCrush.(none)>
2014-02-26 15:13:57 -0700

    2

--- app.js
+++ app.js
@@ -168,6 +168,37 @@ auth.deseralizeUser = function(obj, done){
  done(null, obj);
 };
 
+
+// -- Auth Control Routes
+
+var AuthController = {
+
+  login: passport.authenticate('local', {
+    successRedirect: '/auth/login/success',
+    failureRedirect: '/auth/login/failure'
+  }),
+
+  loginSuccess: function(req, res){
+    res.json({
+      success: true,
+      user: req.session.passport.user
+    });
+  },
+
+  loginFailure: function(req, res){
+    res.json({
+      success:false,
+      message: 'Invalid username or password.'
+    });
+  },
+
+  logout: function(req, res){
+    req.logout();
+    res.end();
+  },
+};
+
+
 // -- Routes -------------------------------------------------------------------
 
 app.get('/', routes.render('home'));
