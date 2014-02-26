079449fe64be459b6f8c07df1d01ad57b6ef2ff0
iweaklim <iweaklim@CoreCrush.(none)>
2014-02-26 00:28:56 -0700

    2

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ app.get('/registry/',
 //basic login page. 
 app.get('/login',
   function(req, res) {
-    res.send('<html><body><a href="/auth/twitter">Sign in with Twitter</a></body></html>');
+    routes.render('landing_page');
   });
 
 app.get( '/rsvp/',                       routes.rsvp.pub, routes.rsvp.edit);
