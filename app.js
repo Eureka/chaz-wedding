12413473ff7eae4c5dfdceb46108b7a1d2b0cd28
iweaklim <iweaklim@CoreCrush.(none)>
2014-02-26 23:25:05 -0700

    d

--- app.js
+++ app.js
@@ -206,6 +206,6 @@ app.get('/combo/:version', [
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login');
-//}
+}
 
 module.exports = app;
