4943faa9a338c1c8756e18f9f8660eab33350bea
Eureka <eureka@fusionnetwork.us>
2014-03-11 00:34:32 -0600

    Fix top nav bar. Add Logout button.

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ app.locals({
         {id: 'logistics', url: '/logistics/', label: 'Logistics'},
         {id: 'registry',  url: '/registry/',  label: 'Registry'},
         {id: 'rsvp',      url: '/rsvp/',      label: 'RSVP'},
-        {id: 'party_itinerary',      url: '/party_itinerary/',      label: 'Itinerary'}
+        {id: 'party_itinerary',      url: '/party_itinerary/',      label: 'Itinerary'},
+        {id: 'logout',      url: '/logout/',      label: 'Logout'}
 
     ],
 
