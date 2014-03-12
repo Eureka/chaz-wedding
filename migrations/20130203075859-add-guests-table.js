e79061d998d6d3a08bcc1221afcd651b2aa7eb10
root <root@test.hacktheory.com>
2014-03-12 12:08:24 -0600

    updates to use guests table as user table

--- migrations/20130203075859-add-guests-table.js
+++ migrations/20130203075859-add-guests-table.js
@@ -12,13 +12,20 @@ exports.up = function (db, callback) {
                     type         : 'int'
                 },
 
-                invitation_id: 'int',
+                invitation_id: {type: 'int'},
 
                 title: {type: 'string', length: 8},
                 name : {type: 'string', length: 64},
                 email: {type: 'string', length: 128},
+		password: {type: 'string', length: 16},
+		groups: {type: 'string', length: 256},
+		invite_key: {type: 'string', length: 8},
+		allergies: {type: 'string', length: 256},
+		food_pref: {type: 'string', length: 256},		
 
                 is_attending: 'boolean',
+		total_attending: {type: 'string', length: 256},
+		attending_names: {type: 'string', length: 256},
                 is_plusone  : {type: 'boolean', defaultValue: false}
             },
 
--- models/authController.js
+++ models/authController.js
@@ -3,38 +3,11 @@ var PassportLocalStrategy = require('passport-local').Strategy;
 var LocalStrategy = require('passport-local').Strategy;
 var User  = require('./userModel.js');
 
-/*
-var AuthController = {
-
-  login: passport.authenticate('local', {
-    successRedirect: '/auth/login/success',
-    failureRedirect: '/auth/login/failure'
-  }),
-
-  loginSuccess: function(req, res){
-    res.json({
-      success: true,
-      user: req.session.passport.user
-    });
-  },
-
-  loginFailure: function(req, res){
-    res.json({
-      success:false,
-      message: 'Invalid username or password.'
-    });
-  },
-
-  logout: function(req, res){
-    req.logout();
-    res.end();
-  },
-};
-
-*/
-
 
 passport.use(new LocalStrategy(function(username, password, done) { 
+// Set system to use email as username.
+ usernameField: 'email',
+ passwordField: 'password',
   // insert your MongoDB check here. For now, just a simple hardcoded check.
   console.log("local envoked");
 // var User = require('./userModel.js').User;
@@ -52,28 +25,6 @@ passport.use(new LocalStrategy(function(username, password, done) {
 
 
 
-/*
-  if (username === 'foo' && password === 'bar')
-  {
-    //done(null, { user: username });
-    console.log("should have passed");
-    console.log(username);
-    console.log(password);
-    //return done(null, false, { message: 'Nobody here by that name'} );
-    done(null, { user: username });
-  }
-  else
-  {
-    //done(null, false);
-    console.log("should have failed");
-    console.log(username);
-    console.log(password);
-    //return done(null, false, { message: 'Wrong password'} );
-    done(null, false);
-  }
-}));
-*/
-
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -81,19 +32,3 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
-/*
- function loggedIn(req, res, next) {
-  if (req.user) {
-    next();
-  } else {
-    res.redirect('/login');
-  }
-};
-
-module.exports = loggedIn;
-
-exports.ensureAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-    res.redirect('/login');
-};
-*/
