7432b1510e8943f9aaa38e044e4a541fdeedb811
Eureka <eureka@fusionnetwork.us>
2014-03-12 18:00:07 -0600

    Working group based auth

--- app.js
+++ app.js
@@ -38,46 +38,6 @@ app.engine('hbs', exphbs({
     partialsDir  : config.dirs.partials
 }));
 
-/*
-// -- Roles middleware -------------------------------------------------------------------
-var user = new ConnectRoles({
-  failureHandler: function (req, res, action) {
-    // optional function to customise code that runs when
-    // user fails authorisation
-    var accept = req.headers.accept || '';
-    res.status(403);
-    if (~accept.indexOf('html')) {
-    	console.log('userGroups: ', user.groups);
-      res.render('access-denied', {action: action});
-    } else {
-      res.send('Access Denied - You don\'t have permission to: ' + action);
-    }
-  }
-});
-
-//anonymous users can only access the home page
-////returning false stops any more rules from being
-////considered
-user.use(function (req, action) {
-  if (!req.isAuthenticated()) return action === 'access home page';
-});
-
-//moderator users can access private page, but
-////they might not be the only ones so we don't return
-////false if the user isn't a moderator
-user.use('access private page', function (req) {
-  if (req.user.role === 'moderator') {
-    return true;
-  }
-});
-
-//admin users can access all pages
-user.use(function (req) {
-  if (user.groups === 'admin') {
-    return true;
-  }
-});
-*/
 
 // -- Locals -------------------------------------------------------------------
 
@@ -166,7 +126,7 @@ var needsGroup = function(group) {
   	//var myData = req.user;
   	//console.log('groupTest: ', myData.group);
   	console.log('group2Test: ', req.user.group);
-    if ( req.user.group === group) {
+    if ( req.user.group === group || req.user.group === admin) {
       next();
       console.log('yay');
     } else {
@@ -190,8 +150,9 @@ app.get('/auth/login/success', routes.render('/wedding/'));
 
 app.get('/admin/',
   ensureLoggedIn('/login'),
+  needsGroup('admin'),
   function(req, res) {
-    res.render('views/pages/admin/admin_main', { user: req.user });
+    res.render('views/pages/admin/admin_main', { user: req.user, groups: req.user.group });
     console.log(req.user);
   });
 
@@ -202,18 +163,9 @@ app.get('/home/',
     res.render('home', { user: req.user });
     console.log(req.user);
   });
-/*
-app.get('/registry/',
-  ensureLoggedIn('/login'),
-  needsGroup('admin'),
-  function(req, res) {
-    res.render('registry', { user: req.user });
-    console.log(req.user);
-  });
-  */
+
  app.get('/registry/',
   ensureLoggedIn('/login'),
-  needsGroup('admin'),
   function(req, res) {
     res.render('registry', { user: req.user, groups: req.user.group });
     console.log(req.user, req.user.group);
@@ -256,8 +208,9 @@ app.get('/wedding/',
  
 app.get('/party_itinerary/',
   ensureLoggedIn('/login'),
+  needsGroup('wedding_party'),
   function(req, res) {
-    res.render('party_itinerary', { user: req.user });
+    res.render('party_itinerary', { user: req.user, groups: req.user.group });
     console.log(req.user);
   }); 
  
