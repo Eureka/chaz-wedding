d68fe4855de53b45a702796bc6979a126c37c90b
Eureka <eureka@fusionnetwork.us>
2014-03-12 18:31:02 -0600

    s

--- app.js
+++ app.js
@@ -247,9 +247,9 @@ app.get('/rsvp/brunch/',
   });
 
 
-//app.get( '/rsvp/',                       routes.rsvp.pub, routes.rsvp.edit);
+app.get( '/rsvp/',                       routes.rsvp.pub, routes.rsvp.edit);
 app.post('/rsvp/',                       routes.rsvp.resend);
-//app.get( '/rsvp/brunch/',                routes.rsvp.brunch);
+app.get( '/rsvp/brunch/',                routes.rsvp.brunch);
 app.post('/rsvp/brunch/',                routes.rsvp.brunch);
 app.get( '/rsvp/brunch/:invitation_key', routes.rsvp.login);
 app.get( '/rsvp/:invitation_key',        routes.rsvp.login);
