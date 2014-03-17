4aa1bec942f01b3eb4e4e396c028221403b02492
Eureka <eureka@fusionnetwork.us>
2014-03-16 20:26:20 -0600

    s

--- app.js
+++ app.js
@@ -214,30 +214,6 @@ app.get('/party_itinerary/',
     console.log(req.user);
   }); 
  
-// Attempt to load data from database for RSVP page. 
-
-app.get('/Userdata', function(req, res){
-   var pg = require('pg'); 
-
-            var conString = "postgres://postgres:open-marriage@localhost/open-marriage";
-
-            var client = new pg.Client(conString);
-            client.connect(function(err) {
-              if(err) {
-                res.send('could not connect to postgres');
-              }
-              client.query('SELECT * from guests where id=1', function(err, result) {
-                if(err) {
-                 res.send('error running query'); 
-                }
-                res.set("Content-Type", 'text/javascript'); // i added this to avoid the "Resource interpreted as Script but transferred with MIME type text/html" message
-                res.send(JSON.stringify(result.rows[0].json));
-                              client.end();
-              });
-            }); 
-
-});
-
 app.get('/rsvp/',
   ensureLoggedIn('/login'),
   function(req, res) {
@@ -265,7 +241,7 @@ app.post('/login', passport.authenticate('local', {
 }));
 
 
-/*
+
 app.get('/rsvp/',
   ensureLoggedIn('/login'),
   routes.rsvp.pub, routes.rsvp.edit,
@@ -273,7 +249,7 @@ app.get('/rsvp/',
     req.render('routes.rsvp.pub, routes.rsvp.edit', { user: req.user });
     console.log(req.user);
   });
-*/
+
 //app.post('/rsvp/',                       routes.rsvp.resend);
 //app.post('/rsvp/brunch/',                routes.rsvp.brunch);
 
